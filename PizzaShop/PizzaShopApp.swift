0792d6f64751274f239a32b9dad923ce9f27f904
Alexey Kolch <kolychenkov94@mail.ru>
2023-10-13 18:27:24 +0400

    Initial Commit

---
+++ PizzaShop/PizzaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaShopApp.swift
+//  PizzaShop
+//
+//  Created by Алексей Колыченков on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
